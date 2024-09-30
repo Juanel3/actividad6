fa7f75b96da49b5c430ddce72f96b40cc24f1732
Hector-Tecmilenio <navarroendy19@gmail.com>
2024-09-29 20:26:23 -0600

    tarea 5

--- actividad6/resources/views/productos/blade1.php
+++
@@ -1,1 +0,0 @@
-<h1>Edición de producto</h1>
--- actividad6/routes/web.php
+++ actividad6/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\ProductoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('productos', ProductoController::class);
+
+
+//correr el servidor
+//http://localhost:8000/productos
+//
+//http://localhost:8000/productos/{id}/edit
